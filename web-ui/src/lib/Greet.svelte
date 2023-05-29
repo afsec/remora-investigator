dd8e55080620e99b31b23ad5490cf3c325497a82
Antonio Costa <97892233+afsec@users.noreply.github.com>
2023-05-29 21:17:28 +0000

    feat: implement basic tauri web-ui

--- web-ui/src/lib/Greet.svelte
+++ web-ui/src/lib/Greet.svelte
@@ -1,19 +1,16 @@
-<script lang="ts">
-  import { invoke } from "@tauri-apps/api/tauri"
+<script lang='ts'>
+  import { invoke } from '@tauri-apps/api/tauri'
 
-  let name = "";
-  let greetMsg = ""
+  let name = ''
+  let greetMsg = ''
 
-  async function greet(){
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
+  async function greet() {
+    greetMsg = await invoke('greet', { name })
   }
 </script>
 
 <div>
-  <form class="row" on:submit|preventDefault={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
+  <input id="greet-input" placeholder="Enter a name..." bind:value="{name}" />
+  <button on:click="{greet}">Greet</button>
   <p>{greetMsg}</p>
 </div>
