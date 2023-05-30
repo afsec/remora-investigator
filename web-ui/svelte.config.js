a0b795cc234989e3cd57dc182e2b7a8fc74b15a6
Antonio Costa <97892233+afsec@users.noreply.github.com>
2023-05-29 21:11:46 -0300

    feat: add Tailwind CSS + DaisyUI support

--- web-ui/svelte.config.js
+++ web-ui/svelte.config.js
@@ -1,12 +1,12 @@
 // import adapter from '@sveltejs/adapter-auto';
-// import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess'
+// import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter()
