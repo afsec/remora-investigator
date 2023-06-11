f6f1ecb44ba380ccc211eec8d94ced7cf9e944d1
Antonio Costa <97892233+afsec@users.noreply.github.com>
2023-06-11 00:30:28 +0000

    feat: implement basic interface for session info

--- src-tauri/src/storage/remora_storage/mod.rs
+++ src-tauri/src/storage/remora_storage/mod.rs
@@ -1,22 +1,76 @@
-mod connection;
+use anyhow::anyhow;
+use sea_orm::{Database, DatabaseConnection};
 
-use sea_orm::DatabaseConnection;
-
-use self::connection::DbConnection;
+use crate::AppResult;
+use crate::SESSIONS_DIR;
 
 #[derive(Debug, Default)]
-pub struct RemoraStorage;
+pub struct RemoraStorage {
+    uri: String,
+    connection: DatabaseConnection,
+}
+
+pub struct RemoraStorageWithUrl {
+    uri: String,
+}
+
+impl RemoraStorageWithUrl {
+    pub async fn build(self) -> anyhow::Result<RemoraStorage> {
+        let Self { uri } = self;
+        let connection: DatabaseConnection = Database::connect(&uri).await?;
+
+        dbg!(&connection);
+        let storage = RemoraStorage { uri, connection };
+        Ok(storage)
+    }
+}
 
 impl RemoraStorage {
-    pub fn new() -> Self {
-        Default::default()
+    pub fn new<T: AsRef<str>>(session_filename: T) -> AppResult<RemoraStorageWithUrl> {
+        let mut session_file_path = SESSIONS_DIR
+            .get()
+            .ok_or(anyhow!("sessions directory not defined"))?
+            .clone();
+
+        session_file_path.push(session_filename.as_ref());
+        session_file_path.set_extension("sqlite3");
+
+        let maybe_session_file_path = session_file_path
+            .to_str()
+            .map(|path_str| path_str.to_string());
+
+        let database_uri = match maybe_session_file_path {
+            Some(filename_path) => format!("sqlite://{}?mode=rwc", filename_path),
+            None => return Err(anyhow!("Wrong session_filename")),
+        };
+
+        dbg!(&database_uri);
+
+        Ok(RemoraStorageWithUrl { uri: database_uri })
     }
-    pub async fn start_db<T: AsRef<str>>(
-        self,
-        session_filename: T,
-    ) -> anyhow::Result<DatabaseConnection> {
-        let db_connection = DbConnection::start(session_filename).await?;
-        let pool = db_connection.take();
-        Ok(pool)
+    pub async fn start_db(&self) -> anyhow::Result<()> {
+        let _ = &self.db_bootstrap().await?;
+        Ok(())
+    }
+
+    async fn db_bootstrap(&self) -> anyhow::Result<()> {
+        let _ = &self.create_tables().await?;
+        Ok(())
+    }
+    async fn create_tables(&self) -> anyhow::Result<()> {
+        use migration::Migrator;
+        use sea_orm_migration::prelude::*;
+        let schema_manager = SchemaManager::new(&self.connection);
+        Migrator::refresh(&self.connection).await?;
+        assert!(schema_manager.has_table("session").await?);
+        Ok(())
+    }
+
+    pub fn uri(&self) -> &str {
+        self.uri.as_ref()
+    }
+
+    pub fn connection(&self) -> &DatabaseConnection {
+        &self.connection
     }
 }
