2c18a396cdcd5b5e9476e0fd4285a6a9ce8fe0c9
jadsn <120682244+JADSN1894@users.noreply.github.com>
2023-06-14 09:49:07 -0300

    fix(play-and-pause-events): Fix toggle play and event after click and use api to show alerts

--- src/lib/Launcher.svelte
+++ src/lib/Launcher.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { invoke } from '@tauri-apps/api/tauri';
+	import { message } from '@tauri-apps/api/dialog';
 
 	import { writable, type Writable } from 'svelte/store';
 	import PlayIcon from './icons/svg/boxicons/PlayIcon.svelte';
@@ -9,9 +10,9 @@
 	let outcomeMsg = '';
 
 	async function launch() {
-		$currentLauncherState = 1;
+		currentLauncherState.set(1);
 		outcomeMsg = await invoke('launch_interceptor', { sessionName });
-		alert(outcomeMsg);
+		await message(outcomeMsg);
 	}
 
 	export const currentLauncherState: Writable<number> = writable(0);
@@ -19,13 +20,11 @@
 
 <div>
 	<div class="flex items-center">
-		<button type="button" class="btn-icon btn-icon-sm" on:click={launch}>
-			{#if $currentLauncherState == 0}
-				<PlayIcon />
-			{/if}
-			{#if $currentLauncherState != 0}
-				<PauseIcon />
-			{/if}
-		</button>
+		{#if $currentLauncherState == 0}
+			<PlayIcon on:onClick={launch} />
+		{/if}
+		{#if $currentLauncherState != 0}
+			<PauseIcon on:onClick={() => currentLauncherState.set(0)} />
+		{/if}
 	</div>
 </div>
